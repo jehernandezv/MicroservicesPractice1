cd9073ef790c634157d3ec95e18f63e7749a78ec
Camilo018 <cristian.lopez04@uptc.edu.co>
2020-08-01 19:04:43 -0500

    Se realizaron multiples modificaciones al aspecto frontend del sistema

--- frontend/src/UI.js
+++ frontend/src/UI.js
@@ -5,8 +5,8 @@ class UI{
         const sentenseDiv = document.getElementById('sentences-div');
         const div = document.createElement('div');
         div.className ='';
-        div.innerHTML=`<div class="card m-2">
-            <h2>${sentense.message}</h2>
+        div.innerHTML=`<div class="card m-2 bg-dark ">
+            <h2 class="text-light text-center">${sentense.message}</h2>
             </div>`;
         sentenseDiv.appendChild(div);
     }
